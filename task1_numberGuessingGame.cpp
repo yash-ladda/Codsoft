aa7725374f4d528f4a131015bef2759cf6b9aa4e
yash-ladda <yashladda7777@gmail.com>
2024-07-01 16:19:49 +0530

    Random number generated between given minimum and maximum limits

--- task1_numberGuessingGame.cpp
+++ task1_numberGuessingGame.cpp
@@ -3,7 +3,17 @@ using namespace std;
 
 int main() {
 
+    srand((unsigned) time(NULL));  // Provided a seed value to generate random number
+
+    int minLimit, maxLimit;
+    cout << "Enter minimum and maximum limits respectively: " << endl;
+    cin >> minLimit >> maxLimit;   // Taking minimum and maximum limits to generate random number
+
+    int range = maxLimit - minLimit + 1; // Defining range
+
+    int random = minLimit + (rand() % range); // Generating random number between range
 
     
+
     return 0;
 }
